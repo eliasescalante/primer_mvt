4e22cdab8275a7ecb9e7d0d6ff21551e2f5f88bf
eliasescalante <eliasw.escalante@gmail.com>
2023-09-24 18:24:51 -0300

    agregue la pagina con los familiares en ella con ciclo for en la misma usando la base de datos

--- primer_mvt/familia/admin.py
+++ primer_mvt/familia/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+# from .models import Familia
 
 # Register your models here.
+# admin.site.register(Familia)
--- primer_mvt/familia/views.py
+++ primer_mvt/familia/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from familia.models import Familia
+# from django.http import HttpResponse
 
-# Create your views here.
+# # Create your views here.
+# def familia(request):
+#     return render ( request, 'familia/tempate/inicio.html')
+
+def home(request):
+
+    familiares = Familia.objects.all()
+
+
+    return render (request, "index.html",{"familiares":familiares})
+
